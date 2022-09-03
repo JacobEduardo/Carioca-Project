bf02e06eeb0ef77b710b84763ed080e6e31f2278
JacobEduardo <jacob.parraguez.vargas@gmail.com>
2022-09-02 21:25:41 -0400

    add main

--- Class/Table.js
+++ Class/Table.js
@@ -9,7 +9,12 @@ class Card{
 class DeckOfCards{
     constructor(){
         this.cards = []
-        this.cards.push(new Card (0,"red","♥"));
+
+        this.cards.push(new Card (0,"red","☺"));
+        this.cards.push(new Card (0,"red","☺"));
+        this.cards.push(new Card (0,"black","☺"));
+        this.cards.push(new Card (0,"black","☺"));
+
         this.cards.push(new Card (1,"red","♥"));
         this.cards.push(new Card (2,"red","♥"));
         this.cards.push(new Card (3,"red","♥"));
@@ -24,7 +29,6 @@ class DeckOfCards{
         this.cards.push(new Card (12,"red","♥"));
         this.cards.push(new Card (13,"red","♥"));
 
-        this.cards.push(new Card (0,"red","♦"));
         this.cards.push(new Card (1,"red","♦"));
         this.cards.push(new Card (2,"red","♦"));
         this.cards.push(new Card (3,"red","♦"));
@@ -39,7 +43,6 @@ class DeckOfCards{
         this.cards.push(new Card (12,"red","♦"));
         this.cards.push(new Card (13,"red","♦"));
 
-        this.cards.push(new Card (0,"black","♣"));
         this.cards.push(new Card (1,"black","♣"));
         this.cards.push(new Card (2,"black","♣"));
         this.cards.push(new Card (3,"black","♣"));
@@ -54,7 +57,62 @@ class DeckOfCards{
         this.cards.push(new Card (12,"black","♣"));
         this.cards.push(new Card (13,"black","♣"));
 
-        this.cards.push(new Card (0,"black","♠"));
+        this.cards.push(new Card (1,"black","♠"));
+        this.cards.push(new Card (2,"black","♠"));
+        this.cards.push(new Card (3,"black","♠"));
+        this.cards.push(new Card (4,"black","♠"));
+        this.cards.push(new Card (5,"black","♠"));
+        this.cards.push(new Card (6,"black","♠"));
+        this.cards.push(new Card (7,"black","♠"));
+        this.cards.push(new Card (8,"black","♠"));
+        this.cards.push(new Card (9,"black","♠"));
+        this.cards.push(new Card (10,"black","♠"));
+        this.cards.push(new Card (11,"black","♠"));
+        this.cards.push(new Card (12,"black","♠"));
+        this.cards.push(new Card (13,"black","♠"));
+///////////////////////////////////////////////////////////////////////
+        this.cards.push(new Card (1,"red","♥"));
+        this.cards.push(new Card (2,"red","♥"));
+        this.cards.push(new Card (3,"red","♥"));
+        this.cards.push(new Card (4,"red","♥"));
+        this.cards.push(new Card (5,"red","♥"));
+        this.cards.push(new Card (6,"red","♥"));
+        this.cards.push(new Card (7,"red","♥"));
+        this.cards.push(new Card (8,"red","♥"));
+        this.cards.push(new Card (9,"red","♥"));
+        this.cards.push(new Card (10,"red","♥"));
+        this.cards.push(new Card (11,"red","♥"));
+        this.cards.push(new Card (12,"red","♥"));
+        this.cards.push(new Card (13,"red","♥"));
+
+        this.cards.push(new Card (1,"red","♦"));
+        this.cards.push(new Card (2,"red","♦"));
+        this.cards.push(new Card (3,"red","♦"));
+        this.cards.push(new Card (4,"red","♦"));
+        this.cards.push(new Card (5,"red","♦"));
+        this.cards.push(new Card (6,"red","♦"));
+        this.cards.push(new Card (7,"red","♦"));
+        this.cards.push(new Card (8,"red","♦"));
+        this.cards.push(new Card (9,"red","♦"));
+        this.cards.push(new Card (10,"red","♦"));
+        this.cards.push(new Card (11,"red","♦"));
+        this.cards.push(new Card (12,"red","♦"));
+        this.cards.push(new Card (13,"red","♦"));
+
+        this.cards.push(new Card (1,"black","♣"));
+        this.cards.push(new Card (2,"black","♣"));
+        this.cards.push(new Card (3,"black","♣"));
+        this.cards.push(new Card (4,"black","♣"));
+        this.cards.push(new Card (5,"black","♣"));
+        this.cards.push(new Card (6,"black","♣"));
+        this.cards.push(new Card (7,"black","♣"));
+        this.cards.push(new Card (8,"black","♣"));
+        this.cards.push(new Card (9,"black","♣"));
+        this.cards.push(new Card (10,"black","♣"));
+        this.cards.push(new Card (11,"black","♣"));
+        this.cards.push(new Card (12,"black","♣"));
+        this.cards.push(new Card (13,"black","♣"));
+
         this.cards.push(new Card (1,"black","♠"));
         this.cards.push(new Card (2,"black","♠"));
         this.cards.push(new Card (3,"black","♠"));
@@ -79,9 +137,9 @@ class Table{
 
     dealPlayer(){
         var x = this.deckOfCards.cards.length
-        let card  = this.deckOfCards.cards[Math.floor(Math.random() * x)]
-        console.log(card)
-        this.deckOfCards.cards.splice(x, 1)
+        var y = Math.floor(Math.random() * x)
+        let card  = this.deckOfCards.cards[y]
+        this.deckOfCards.cards.splice(y, 1)
         return card
     }
 }
--- Class/main.js
+++ Class/main.js
@@ -7,15 +7,37 @@ const q1 = readline.createInterface({
     output: process.stdout,
 });
 
+const players = []
+
 //////////////////////////////////////////////////
 
 var table = new _table.Table()
-var playar01 = new _player.Player()
 
 q1.question("Cuantos van jugar", function (answer) {
-    playar01.hand.push (table.dealPlayer())
-    q1.close();
-});
 
-console.log("este player")
-console.log(playar01.hand)
+    for (let i = 0; i < answer; i++) {
+        players[i] = new _player.Player("plater_0" + i.toString());
+        for (let k = 0; k < 12; k++) {
+            players[i].hand.push(table.dealPlayer())
+        }
+        console.log(players[i].hand.length)
+    }
+
+    console.log("este player")
+    console.log(players)
+    console.log(table.deckOfCards.cards.length)
+
+    q1.question("Quien parte?", function (answer) {
+        i = answer
+        for (let i = 0; i < players.length; i++) {
+            players[i].hand.push(table.dealPlayer())
+            console.log(players[i].hand.length)
+            if (i = players.length) {
+                i = 0
+            }
+            if(table.deckOfCards.cards.length == 0){
+                i = 100
+            }
+        }
+    });
+});
